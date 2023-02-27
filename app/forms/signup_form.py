b889e39a2292e79219b1fbd3e19ae32fd6269986
justin chau <jchau623@gmail.com>
2023-02-27 13:29:33 -0500

    backend forms

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Length
 from app.models import User
 
 
@@ -22,6 +22,6 @@ def username_exists(form, field):
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+        'username', validators=[DataRequired('Username is required.'), username_exists,Length(min=3, max=40, message='Username must be between 3 and 30 characters.')])
+    email = StringField('email', validators=[DataRequired('Email address is required.'), user_exists, Length(min=3, max=255, message='Email must be between 3 and 255 characters.')])
+    password = StringField('password', validators=[DataRequired('Password is required.')])
