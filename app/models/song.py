3c59ba023a358d63060dad928fe17283f494cb22
justin chau <jchau623@gmail.com>
2023-03-07 11:26:03 -0500

    tables fixed

--- app/models/song.py
+++ app/models/song.py
@@ -5,13 +5,13 @@ class Song(db.Model):
     __tablename__ = 'songs'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
     album_id = db.Column(db.Integer, db.ForeignKey('albums.id'))
-    song_url = db.Column(db.String(), nullable=False)
-    image_url = db.Column(db.String())
-    track_number = db.Column(db.Integer) 
+    song_url = db.Column(db.String(500), nullable=False)
+    image_url = db.Column(db.String(500))
+    track_number = db.Column(db.Integer)
 
     user = db.relationship('User', back_populates='songs')
     artist = db.relationship('Artist', back_populates='songs')
