77524bd182bfff90aa94fb60a39143b21259c1f3
justin chau <jchau623@gmail.com>
2023-03-09 14:15:23 -0500

    library redux

--- react-app/src/components/HomePage.js/index.js
+++ react-app/src/components/HomePage.js/index.js
@@ -6,6 +6,12 @@ import './HomePage.css'
 export default function HomePage() {
 
     const dispatch = useDispatch()
+    const playlists = useSelector(state => state?.playlists?.list)
+    console.log(playlists)
+    const sessionUser = useSelector(state => state?.session?.user)
+    console.log(sessionUser)
+    const songs = useSelector(state => state?.songs?.list)
+    
 
     return (
         <div className='page-container'>
