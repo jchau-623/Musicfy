71e56b0dbf9e05a41f6efe15a03326aed7683936
justin chau <jchau623@gmail.com>
2024-01-03 11:43:28 -0500

    revert

--- react-app/src/components/HomePage.js/index.js
+++ react-app/src/components/HomePage.js/index.js
@@ -1,24 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
-import AlbumPlayer from '../AlbumPlayer';
+import NavBar from '../NavBar';
 import SongPlayer from '../SongPlayer';
-import './Home.css';
-import HomeSidebar from './HomeSidebar';
-import TileCarousel from '../TileCarousel';
 import { getNewAlbumsArray, getNewSongsArray, getTopAlbumsArray, loadHome, setLoaded } from '../../store/home';
 import { getPlaylists } from '../../store/playlists';
-import MainSidebar from '../MainSidebar';
+import './HomePage.css'
 
-export default function Home() {
+export default function HomePage() {
+
     const dispatch = useDispatch();
 
     const [homeDisplay, setHomeDisplay] = useState('albums');
     const homeItems = useSelector(({ home }) => home);
-    const newSongs = useSelector(getNewSongsArray);
-    const newAlbums = useSelector(getNewAlbumsArray);
-    const featuredAlbum = homeItems.featuredAlbum;
+    // const newSongs = useSelector(getNewSongsArray);
+    // const newAlbums = useSelector(getNewAlbumsArray);
     const featuredSongs = Object.values(homeItems.featuredSongs)
-    const topAlbums = useSelector(getTopAlbumsArray);
+
 
     useEffect(() => {
         if (homeItems.isLoaded) return;
@@ -32,11 +29,10 @@ export default function Home() {
         homeContent = (
             <>
                 <h2>Featured Album</h2>
-                <AlbumPlayer album={featuredAlbum} />
+
                 <h2>New Albums</h2>
-                <TileCarousel content={newAlbums} option='albums' identifier='newAlbums'/>
+
                 <h2>Top Albums</h2>
-                <TileCarousel content={topAlbums} option='albums'identifier='topAlbums'/>
             </>
         )
     } else if (homeDisplay === 'songs') {
@@ -50,22 +46,18 @@ export default function Home() {
                     ))}
                 </div>
                 <h2>New Songs</h2>
-                <TileCarousel content={newSongs} option='songs'/>
             </>
         )
     }
 
     return (
-        <div id='home-wrapper'>
-            <div className='main-wrapper'>
-                {homeContent}
-            </div>
 
-            <div className='sidebar-container'>
-                <MainSidebar />
-
-                <HomeSidebar setHomeDisplay={setHomeDisplay} homeDisplay={homeDisplay}/>
-            </div>
+        <div className='page-container'>
+        <NavBar />
+        <div className='page-content'>
+            {homeContent}
         </div>
+    </div>
+//
     )
-};
+}
