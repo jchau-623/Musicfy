f91c3a44425f52baf4e6268a3bbef11d814ca0a1
justin chau <jchau623@gmail.com>
2024-01-03 11:33:59 -0500

    home page

--- react-app/src/components/HomePage.js/index.js
+++ react-app/src/components/HomePage.js/index.js
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
-import NavBar from '../NavBar';
+import AlbumPlayer from '../AlbumPlayer';
 import SongPlayer from '../SongPlayer';
+import './Home.css';
+import HomeSidebar from './HomeSidebar';
+import TileCarousel from '../TileCarousel';
 import { getNewAlbumsArray, getNewSongsArray, getTopAlbumsArray, loadHome, setLoaded } from '../../store/home';
 import { getPlaylists } from '../../store/playlists';
-import './HomePage.css'
+import MainSidebar from '../MainSidebar';
 
-export default function HomePage() {
-
+export default function Home() {
     const dispatch = useDispatch();
 
     const [homeDisplay, setHomeDisplay] = useState('albums');
     const homeItems = useSelector(({ home }) => home);
-    // const newSongs = useSelector(getNewSongsArray);
-    // const newAlbums = useSelector(getNewAlbumsArray);
+    const newSongs = useSelector(getNewSongsArray);
+    const newAlbums = useSelector(getNewAlbumsArray);
+    const featuredAlbum = homeItems.featuredAlbum;
     const featuredSongs = Object.values(homeItems.featuredSongs)
-
+    const topAlbums = useSelector(getTopAlbumsArray);
 
     useEffect(() => {
         if (homeItems.isLoaded) return;
@@ -29,10 +32,11 @@ export default function HomePage() {
         homeContent = (
             <>
                 <h2>Featured Album</h2>
-
+                <AlbumPlayer album={featuredAlbum} />
                 <h2>New Albums</h2>
-
+                <TileCarousel content={newAlbums} option='albums' identifier='newAlbums'/>
                 <h2>Top Albums</h2>
+                <TileCarousel content={topAlbums} option='albums'identifier='topAlbums'/>
             </>
         )
     } else if (homeDisplay === 'songs') {
@@ -46,18 +50,22 @@ export default function HomePage() {
                     ))}
                 </div>
                 <h2>New Songs</h2>
+                <TileCarousel content={newSongs} option='songs'/>
             </>
         )
     }
 
     return (
+        <div id='home-wrapper'>
+            <div className='main-wrapper'>
+                {homeContent}
+            </div>
 
-        <div className='page-container'>
-        <NavBar />
-        <div className='page-content'>
-            {homeContent}
+            <div className='sidebar-container'>
+                <MainSidebar />
+
+                <HomeSidebar setHomeDisplay={setHomeDisplay} homeDisplay={homeDisplay}/>
+            </div>
         </div>
-    </div>
-//
     )
-}
+};
--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -1,6 +1,21 @@
+import { cleanActive } from "./active";
+import { cleanHome } from "./home";
+import { cleanLibraryAlbums } from "./library/libraryAlbums";
+import { cleanLibrarySongs } from "./library/librarySongs";
+import { cleanPlaylists } from "./playlists";
+
 // constants
 const SET_USER = 'session/SET_USER';
 const REMOVE_USER = 'session/REMOVE_USER';
+
+const ADD_SONG_LIKE = 'session/ADD_SONG_LIKE';
+const REMOVE_SONG_LIKE = 'session/REMOVE_SONG_LIKE';
+const ADD_ALBUM_LIKE = 'session/ADD_ALBUM_LIKE';
+const REMOVE_ALBUM_LIKE = 'session/REMOVE_ALBUM_LIKE';
+
+const ADD_USER_ALBUM = 'session/ADD_USER_ALBUM';
+const REMOVE_USER_ALBUM = 'session/REMOVE_USER_ALBUM';
+
 
 const setUser = (user) => ({
   type: SET_USER,
@@ -10,6 +25,34 @@ const setUser = (user) => ({
 const removeUser = () => ({
   type: REMOVE_USER,
 })
+
+const addSongLike = (songId) => {
+  return {
+    type: ADD_SONG_LIKE,
+    songId,
+  };
+};
+
+const removeSongLike = (songId) => {
+  return {
+    type: REMOVE_SONG_LIKE,
+    songId
+  };
+};
+
+const addAlbumLike = (albumId) => {
+  return {
+    type: ADD_ALBUM_LIKE,
+    albumId
+  };
+};
+
+const removeAlbumLike = (albumId) => {
+  return {
+    type: REMOVE_ALBUM_LIKE,
+    albumId
+  }
+};
 
 export const loadUserAlbum = (album) => {
   return {
@@ -25,11 +68,6 @@ export const removeUserAlbum = (albumId) => {
   }
 }
 
-const ADD_USER_ALBUM = 'session/ADD_USER_ALBUM';
-const REMOVE_USER_ALBUM = 'session/REMOVE_USER_ALBUM';
-
-const initialState = { user: null };
-
 export const authenticate = () => async (dispatch) => {
   const response = await fetch('/api/auth/', {
     headers: {
@@ -41,8 +79,7 @@ export const authenticate = () => async (dispatch) => {
     if (data.errors) {
       return;
     }
-
-    dispatch(setUser(data));
+    dispatch(setUser(data))
   }
 }
 
@@ -82,12 +119,19 @@ export const logout = () => async (dispatch) => {
   });
 
   if (response.ok) {
-    dispatch(removeUser());
+    await Promise.all([
+      dispatch(cleanLibrarySongs()),
+      dispatch(cleanLibraryAlbums()),
+      dispatch(cleanActive()),
+      dispatch(cleanHome()),
+      dispatch(cleanPlaylists()),
+      dispatch(removeUser()),
+    ])
   }
 };
 
 
-export const signUp = (username, email, password, repeat_email) => async (dispatch) => {
+export const signUp = (username, email, password) => async (dispatch) => {
   const response = await fetch('/api/auth/signup', {
     method: 'POST',
     headers: {
@@ -97,7 +141,6 @@ export const signUp = (username, email, password, repeat_email) => async (dispat
       username,
       email,
       password,
-      repeat_email
     }),
   });
 
@@ -115,9 +158,47 @@ export const signUp = (username, email, password, repeat_email) => async (dispat
   }
 }
 
+export const createSongLike = (songId) => async dispatch => {
+  const res = await fetch(`/api/song_likes/songs/${songId}`, { method: 'POST' });
+
+  if (res.ok) {
+    dispatch(addSongLike(songId))
+  }
+};
+
+export const deleteSongLike = (songId) => async dispatch => {
+  const res = await fetch(`/api/song_likes/songs/${songId}`, { method: 'DELETE' });
+
+  if (res.ok) {
+    dispatch(removeSongLike(songId))
+  }
+};
+
+export const createAlbumLike = (albumId) => async dispatch => {
+  const res = await fetch(`/api/album_likes/albums/${albumId}`, { method: 'POST' });
+
+  if (res.ok) {
+    dispatch(addAlbumLike(albumId))
+  }
+};
+
+export const deleteAlbumLike = (albumId) => async dispatch => {
+  const res = await fetch(`/api/album_likes/albums/${albumId}`, { method: 'DELETE' });
+
+  if (res.ok) {
+    dispatch(removeAlbumLike(albumId))
+  }
+};
+
+
+const initialState = { user: null };
+
 export default function reducer(state = initialState, action) {
   let stateCopy;
+  let songLikes;
+  let albumLikes;
   let idx;
+
   switch (action.type) {
     case SET_USER:
       return { user: action.payload }
@@ -125,6 +206,34 @@ export default function reducer(state = initialState, action) {
       return { user: null }
     default:
       return state;
+
+    case ADD_SONG_LIKE:
+      stateCopy = {...state};
+      songLikes = state.user.liked.song_ids;
+      songLikes.push(action.songId);
+      return stateCopy;
+
+    case REMOVE_SONG_LIKE:
+      stateCopy = {...state};
+      songLikes = state.user.liked.song_ids;
+      idx = songLikes.findIndex(id => id === action.songId);
+      songLikes.splice(idx, 1);
+
+      return stateCopy;
+
+    case ADD_ALBUM_LIKE:
+      stateCopy = { ...state };
+      albumLikes = state.user.liked.album_ids;
+      albumLikes.push(action.albumId);
+      return stateCopy;
+
+    case REMOVE_ALBUM_LIKE:
+      stateCopy = { ...state };
+      albumLikes = state.user.liked.album_ids;
+      idx = albumLikes.findIndex(id => id === action.albumId);
+      albumLikes.splice(idx, 1);
+      return stateCopy;
+
     case ADD_USER_ALBUM:
       return {
         ...state,
@@ -135,7 +244,7 @@ export default function reducer(state = initialState, action) {
       };
 
     case REMOVE_USER_ALBUM:
-      stateCopy = { ...state };
+      stateCopy = {...state};
       const albums = stateCopy.user.albums;
       idx = albums.findIndex(album => album.id === action.albumId);
       albums.splice(idx, 1);
